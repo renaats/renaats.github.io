9384129db14849fa225fc946c3ba3d10cf7b4b04
Renats <R.Jursevskis@student.tudelft.nl>
2021-12-06 05:04:26 +0100

    Add mobile navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,14 @@
 		});
 
 })(jQuery);
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function toggleMenu() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
